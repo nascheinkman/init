4a16384bdb962d9603ad74f964ef969e79e33bac
Nathan Scheinkman <nascheinkman@protonmail.com>
2024-12-15 11:51:54 -0800

    Add git management to script

--- notes/qubes-ssh-add.sh
+++ notes/qubes-ssh-add.sh
@@ -11,6 +11,15 @@ if [ "$SSH_VAULT_VM" != "" ]; then
     rm -f "$SSH_SOCK"
     sudo -u user /bin/sh -c "umask 177 && exec socat 'UNIX-LISTEN:$SSH_SOCK,fork' 'EXEC:qrexec-client-vm $SSH_VAULT_VM qubes.SshAgent'" &
     export SSH_AUTH_SOCK="/home/user/.SSH_AGENT_$SSH_VAULT_VM"
+    export QUBES_GPG_DOMAIN="$SSH_VAULT_VM"
+    cat <<EOF > ${HOME}/.gitconfig
+[user]
+    email = nascheinkman@protonmail.com
+    name = Nathan Scheinkman
+    signing-key = A3A42E70696DC3F33C268CF3F04A6C7F7ADEC53F
+[gpg]
+    program = qubes-gpg-client-wrapper
+EOF
 else
     echo "Missing VMNAME in argument"
 fi
